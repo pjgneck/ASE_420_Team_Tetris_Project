0335f72b893e23d728189fecf1b58c413df5deb8
Denver Hogan <hogandenver05@gmail.com>
2025-10-23 00:53:14 -0400

    allow random block rotation on spawn

--- tetris_boom/game/block_factory.py
+++ tetris_boom/game/block_factory.py
@@ -1,4 +1,6 @@
+import random
 from game.block import Block
+from game.data import SHAPES
 
 class BlockFactory:
     def create_block(self, x: int = 3, y: int = 0) -> Block:
@@ -9,4 +11,7 @@ class BlockFactory:
         :param y: The starting y-coordinate on the board (default: 0)
         :return: A new Block instance
         """
-        return Block(x, y)
+        block = Block(x, y)
+        num_rotations = len(SHAPES[block.shape])
+        block.rotate(random.randint(0, num_rotations - 1))
+        return block
