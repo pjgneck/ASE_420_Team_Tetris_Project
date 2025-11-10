0bbe000490bf2c0a08a44914b212979de87eaca6
pjgneck <pjgneck@gmail.com>
2025-11-10 10:07:20 -0500

    Update build.py

--- build.py
+++ build.py
@@ -2,18 +2,20 @@ import os
 import platform
 import PyInstaller.__main__
 
+#Get file's Root path
+project_root = os.path.dirname(os.path.abspath(__file__))
 # detect the correct path separator
 sep = ";" if platform.system() == "Windows" else ":"
 
 # construct the add-data arguments
 add_data = [
-    f"tetris_boom/assets{sep}tetris_boom/assets",
-    f"tetris_boom/game{sep}tetris_boom/game",
+    f"{os.path.join(project_root, 'tetris_boom', 'assets')}{sep}tetris_boom/assets",
+    f"{os.path.join(project_root, 'tetris_boom', 'game')}{sep}tetris_boom/game",
 ]
 
 # build arguments
 args = [
-    "tetris_boom/main.py",
+    f"{os.path.join(project_root, 'tetris_boom', 'main.py')}",
     "--onefile",
     "--noconsole",
     "--name=TetrisBOOM",
